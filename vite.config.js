633ba7efc5716121a47427938d367e8b65b0cc94
Saarthak K Goyal <saarthakkgoyal2011@gmail.com>
2026-09-21 18:18:29 +0530

    SEO settings and sitemap edition

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    Sitemap({
+      hostname: 'https://sdc.medicaps.ac.in',
+      // Explicitly tell Google exactly what pages exist on your new site
+      dynamicRoutes: [
+        '/about',
+        '/team',
+        '/careers',
+        '/events',
+        '/projects'
+      ],
+      exclude: [
+        '/google2b1145d466d0733b', 
+        '/google2b1145d466d0733b.html'
+      ],
+      // Optional: If you don't use extensions on your new paths
+      changefreq: 'monthly',
+    }),
+  ],
+  // This block is to optimize bundle chunks 
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'vendor',
+              test: /node_modules/, // Separates npm packages into their own file
+            },
+          ],
+        },
+      },
+    },
+  },
 })
